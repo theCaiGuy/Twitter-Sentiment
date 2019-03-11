8a6bb09cb7a192742ced1db202f801678c490f33
theCaiGuy <michaelcai09@gmail.com>
2019-03-11 00:25:32 -0700

    Added a more expressive logging to base model

--- run.py
+++ run.py
@@ -51,7 +51,7 @@ PAD = '<pad>'
 
 TWEET_LEN = 20
 EMBED_LEN = 100
-N_EPOCHS = 500
+N_EPOCHS = 150
 
 # torch.manual_seed(SEED)
 # torch.cuda.manual_seed(SEED)
@@ -62,6 +62,7 @@ PATH = './data/training.1600000.processed.noemoticon.csv'
 # PATH = './data/train_mini.csv'
 MODEL_PATH = './model_cache/cache'
 
+iter = 0
 
 def vectorize(examples, tok2id):
     vec_examples = []
@@ -185,6 +186,10 @@ def train(model, train_loader, optimizer, criterion):
         epoch_loss += loss.item()
         epoch_acc += acc.item()
 
+        print ("| Iteration: " + str(iter + 1) + " | Train Loss: " + str(loss.item()) + " | Train Acc: " + str(acc.item()) + " |")
+        file = open("./train_loss.txt", "a")
+        file.write("epoch: " + str(epoch + 1) + " iter: " + str(iter + 1) + " loss: " + str(loss.item())  + " accuracy: " + str(acc.item()) + '\n')
+
     return epoch_loss / len(train_loader), epoch_acc / len(train_loader)
 
 
@@ -244,21 +249,19 @@ def train_model(train_dataset, dev_dataset, embeddings_matrix):
 
         train_loss, train_acc = train(CNN_model, train_loader, optimizer, criterion)
 
-        print ('| Epoch: ' + str(epoch + 1) + " | Train Loss: " + str(train_loss) + " | Train Acc: " + str(train_acc) + " |")
-        #print (f'| Epoch: {epoch+1:02} | Train Loss: {train_loss:.3f} | Train Acc: {train_acc*100:.2f}% |')
-        file = open("./losses.txt", "a")
-        file.write("epoch: " + str(epoch + 1) + " train_loss: " + str(train_loss) + '\n')
+        print ('Epoch ' + str(epoch + 1) + " completed. Average minibatch train loss: " + str(train_loss) + ". Average minibatch train acc: " + str(train_acc))
 
-        if (epoch + 1) % 10 == 0:
-            print ("Begin validation...")
-            valid_loss, valid_acc = evaluate(CNN_model, dev_loader, criterion)
-            #print (f'| Val. Loss: {valid_loss:.3f} | Val. Acc: {valid_acc*100:.2f}% |')
-            print ("| Val. Loss: " + str(valid_loss) + " | Val. Acc: " + str(valid_acc) + " |")
+        print ("Begin validation...")
+        valid_loss, valid_acc = evaluate(CNN_model, dev_loader, criterion)
+        print ("| Val. Loss: " + str(valid_loss) + " | Val. Acc: " + str(valid_acc) + " |")
+        file = open("./val_loss.txt", "a")
+        file.write("epoch: " + str(epoch + 1) + " iter: " + str(iter + 1) + " loss: " + str(valid_loss) + " accuracy: " + str(valid_acc) + '\n')
 
-            if valid_acc > best_valid_acc:
-                torch.save(CNN_model.state_dict(), MODEL_PATH)
-                torch.save(optimizer.state_dict(), MODEL_PATH + '.optim')
-                best_valid_acc = valid_acc
+        if valid_acc > best_valid_acc:
+            print ("New top validation accuracy achieved! Saving model params...")
+            torch.save(CNN_model.state_dict(), MODEL_PATH)
+            torch.save(optimizer.state_dict(), MODEL_PATH + '.optim')
+            best_valid_acc = valid_acc
 
     print ("Training complete! Best validation accuracy = " + str(best_valid_acc))
 
